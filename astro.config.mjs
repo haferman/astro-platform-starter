e0ffee6a98330b9c307fa25ab032191b12f0a862
Jeff Haferman <jeff.haferman@gmail.com>
2025-07-19 22:38:51 -0700

    no adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
-import staticAdapter from '@astrojs/adapter-static';
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  integrations: [
-    react()
-  ],
-  adapter: staticAdapter(),
+  integrations: [react()],
   experimental: {
     inlineStyles: 'always'
   }
