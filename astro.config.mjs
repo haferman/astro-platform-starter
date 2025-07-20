df8b915212faa9204a1d0ab258da002714bc8d12
Jeff Haferman <jeff.haferman@gmail.com>
2025-07-19 22:06:32 -0700

    added astrojs/critters

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,17 @@ import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import critters from '@astrojs/critters'; 
 
 // https://astro.build/config
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()]
     },
-    integrations: [react()],
+    integrations: [
+        react(),
+        critters() 
+    ],
     adapter: netlify()
 });
+
