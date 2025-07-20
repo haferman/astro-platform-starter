3de576893d824b91913c86aba3abb63da52ad752
Jeff Haferman <jeff.haferman@gmail.com>
2025-07-19 22:23:30 -0700

    inline CSS without plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import staticAdapter from '@astrojs/adapter-static';
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
   integrations: [react()],
-  adapter: netlify(),
+  adapter: staticAdapter(),
   experimental: {
     inlineStyles: 'always'
   }
