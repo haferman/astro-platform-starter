42177ca1ff0d255d9c8e627504d05296dd17ac7c
Jeff Haferman <jeff.haferman@gmail.com>
2025-07-19 22:50:20 -0700

    completely revert

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
-import staticAdapter from '@astrojs/adapter-static';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()]
   },
-  adapter: staticAdapter()
+  adapter: netlify()
 });
