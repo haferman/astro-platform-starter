f08f1841988ff4cbd8dca02ee07f21387251e9fa
Jeff Haferman <jeff.haferman@gmail.com>
2025-07-19 22:17:44 -0700

    inline all CSS to html

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,14 @@ import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
-import critters from '@astrojs/critters'; 
 
-// https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()]
-    },
-    integrations: [
-        react(),
-        critters() 
-    ],
-    adapter: netlify()
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  integrations: [react()],
+  adapter: netlify(),
+  experimental: {
+    inlineStyles: 'always'
+  }
 });
-
